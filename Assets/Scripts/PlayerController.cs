947754baf565434034f113ac6c9147fef8ce0253
Sebastián B. López <zupremacia@gmail.com>
2026-01-23 15:08:43 -0600

    Borders y Navecita

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,26 +3,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private int _playerSpeed;
+    // Variables de movimiento
+    [Header("Movimiento")]
+    [SerializeField] private float _playerSpeed;
     [SerializeField] private float _smoothTime;
+    [SerializeField] private float _speedMultiplier;
+    private float _newSpeed;
+
+    // Lógica de movimiento
     private Vector2 _input;
     private Vector2 _fixedInput;
     private Vector2 _currentVelocity = Vector2.zero;
+    private float _isRunning;
 
+    // Componentes
     private Rigidbody2D _rb2D;
     private PlayerInput _playerInput;
 
     void Start()
     {
+        // Asignación de componentes
         _rb2D = GetComponent<Rigidbody2D>();
         _playerInput = GetComponent<PlayerInput>();
     }
 
-    void Update()
+    private void Update()
     {
         ReadInput();
-
-        Debug.Log("Input: " + _input);
+        ApplySpeed();
     }
 
     private void FixedUpdate()
@@ -32,20 +40,28 @@ public class PlayerController : MonoBehaviour
 
     private void ReadInput()
     {
+        // Leer el input crudo
         _input = _playerInput.actions["Move"].ReadValue<Vector2>();
+        _isRunning = _playerInput.actions["Run"].ReadValue<float>();
+    }
+
+    private void ApplySpeed()
+    {
+        if (_isRunning != 0)
+        {
+            _newSpeed = _playerSpeed * _speedMultiplier;
+        }
+        else
+        {
+            _newSpeed = _playerSpeed;
+        }
     }
 
     private void Move()
     {
-        // transform.position += new Vector3(_input.x, _input.y, 0) * _playerSpeed * Time.deltaTime;
-
-        // _rb2D.linearVelocity = new Vector3(_input.x, _input.y, 0) * _playerSpeed;
-
-        // _rb2D.MovePosition(_rb2D.position + new Vector2(_input.x, _input.y) * _playerSpeed * Time.fixedDeltaTime);
-
-        // _rb2D.AddForce(new Vector2(_input.x, _input.y) * _playerSpeed);
-
+        // Suavizar el input mediante SmoothDamp
+        // SmoothDamp(posActual, posObjetivo, ref velActual, tiempoLlegar)
         _fixedInput = Vector2.SmoothDamp(_fixedInput, _input, ref _currentVelocity, _smoothTime);
-        _rb2D.MovePosition(_rb2D.position + _fixedInput * _playerSpeed * Time.fixedDeltaTime);
+        _rb2D.MovePosition(_rb2D.position + _fixedInput * _newSpeed * Time.fixedDeltaTime);
     }
 }
